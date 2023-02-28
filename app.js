d4a23e804eaeac92fb600fdeab9d95dc7170a975
Vitalik Kolesnikov <webkolesnikov77@gmail.com>
2023-02-28 14:16:32 +0300

    test2

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 'use strict'
 
-alert('hello world!')
+let money = 40000;
+let income = 'Фриланс'
+let addExpenses = 'Интернет, Такси, Кино'
+let deposit = true
+let mission = 500000
+let period = 12
+
+alert('hello world!');
+
+console.log('hello world!');
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев, цель накопить ${mission}`);
+console.log(addExpenses.split(' , '))
+console.log(addExpenses.toLowerCase());
+
+let budgetDay = money / 30
+console.log(Math.round(budgetDay));
+
+money = prompt('Ваш месячный доход?', 40000)
+
+
+
